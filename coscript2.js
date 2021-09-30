1bb20ed21e5dcdc839e66d679840d02318fac436
Rachana1104 <87826210+Rachana1104@users.noreply.github.com>
2021-09-30 13:43:00 +0530

    version2

--- coscript2.js
+++ coscript2.js
@@ -19,7 +19,6 @@ function apifetch() {
 }
 apifetch();
 
-
 function table(data) {
 
     if (data.countries_stat.length >= 0) {
@@ -41,32 +40,89 @@ function table(data) {
         alert("Message");
 }
 
+const searchFun = () => {
+    let filter = document.getElementById('mySearch').value.toUpperCase();
+    let myTable = document.getElementById('myTable');
+    let tr = document.getElementsByTagName('tr');
+    for (var i = 0; i < tr.length; i++) {
+        let td = tr[i].getElementsByTagName('td')[0];
+
+        if (td) {
+            let textvalue = td.textContent || td.innerHTML;
+            if (textvalue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
 function sorting(data) {
 
     jQuery(document).ready(function($) {
         $('th').on('click', function() {
 
             var column = $(this).data('column')
-            console.log($);
-            console.log('Clicked', column);
+            var order = $(this).data('order')
+            var text = $(this).html()
+            text = text.substring(0, text.length - 1)
 
-            function compare(a, b) {
+            console.log('Clicked', column, order);
+            if (order == 'desc') {
 
-                var first = parseInt(a[column].replace(/,/g, '').replace(/(n\/a)+/gi, '0'));
-                var second = parseInt(b[column].replace(/,/g, '').replace(/(n\/a)+/gi, '0'));
+                $(this).data('order', "asc")
+                if (column == "country") {
+                    console.log(data.countries_stat)
+                    data.countries_stat = data.countries_stat.sort((a, b) => a.country_name > b.country_name ? 1 : -1)
+                    table(data);
+                } else {
+                    function compare(a, b) {
 
-                if (first < second) {
-                    return -1;
-                } else if (first > second) {
-                    return 1;
+                        var first = parseInt(a[column].replace(/,/g, '').replace(/(n\/a)+/gi, '0'));
+                        var second = parseInt(b[column].replace(/,/g, '').replace(/(n\/a)+/gi, '0'));
+
+                        if (first < second) {
+                            return -1;
+                        } else if (first > second) {
+                            return 1;
+                        }
+                        return 0;
+
+                    }
+                    text += '&#9660'
+                    let xyz = data.countries_stat.sort(compare);
+                    console.log(xyz);
+                    table(data);
                 }
-                return 0;
+
+            } else {
+                $(this).data('order', "desc")
+                if (column == "country") {
+                    console.log(data.countries_stat)
+                    data.countries_stat = data.countries_stat.sort((a, b) => a.country_name > b.country_name ? -1 : 1)
+                    table(data);
+                } else {
+                    function compare(a, b) {
+
+                        var first = parseInt(a[column].replace(/,/g, '').replace(/(n\/a)+/gi, '0'));
+                        var second = parseInt(b[column].replace(/,/g, '').replace(/(n\/a)+/gi, '0'));
+
+                        if (first > second) {
+                            return -1;
+                        } else if (first < second) {
+                            return 1;
+                        }
+                        return 0;
+
+                    }
+                    text += '&#9650'
+                    let xyz = data.countries_stat.sort(compare);
+                    console.log(xyz);
+                    table(data);
+                }
             }
-
-            let xyz = data.countries_stat.sort(compare);
-            console.log(xyz);
-            table(data);
-
         });
     });
 }
